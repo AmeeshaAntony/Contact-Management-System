7d0b113b5095f1ec1f6940b16411df23ad026810
AmeeshaAntony <ameesha468@gmail.com>
2025-05-10 14:22:53 +0530

    Frontend Assignment

--- RapidRise-2025-FrontEnd-Assignment/frontend/src/components/DashboardReactComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function DashboardReactComponent() {
-  return <h3>This is a React Component in the Dashboard</h3>;
-}
-
-export default DashboardReactComponent; 
--- RapidRise-2025-FrontEnd-Assignment/frontend/src/reactApp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-function ReactComponent() {
-  return <h2></h2>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById('react-root'));
-root.render(<ReactComponent />);
